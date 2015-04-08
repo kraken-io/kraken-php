36f5e247330fcc9382127d0148e4afad48580ead
Przemek Matylla <przemek@matylla.pl>
2015-04-08 12:04:19 +0200

    Fix encoding issues by specifying Content-Type for the 'URL' route

--- lib/Kraken.php
+++ lib/Kraken.php
@@ -1,12 +1,9 @@
 <?php
 
-class Kraken
-{
-
+class Kraken {
     protected $auth = array();
 
-    public function __construct($key = '', $secret = '')
-    {
+    public function __construct($key = '', $secret = '') {
         $this->auth = array(
             "auth" => array(
                 "api_key" => $key,
@@ -15,41 +12,37 @@ class Kraken
         );
     }
 
-    public function url($opts = array())
-    {
+    public function url($opts = array()) {
         $data = json_encode(array_merge($this->auth, $opts));
-        $response = self::request($data, "https://api.kraken.io/v1/url");
+        $response = self::request($data, 'https://api.kraken.io/v1/url', 'url');
 
         return $response;
     }
 
-    public function upload($opts = array())
-    {
-        if (!isset($opts['file']))
-        {
+    public function upload($opts = array()) {
+        if (!isset($opts['file'])) {
             return array(
                 "success" => false,
                 "error" => "File parameter was not provided"
             );
         }
 
-        if (preg_match("/\/\//i", $opts['file']))
-        {
+        if (preg_match("/\/\//i", $opts['file'])) {
             $opts['url'] = $opts['file'];
             unset($opts['file']);
+
             return $this->url($opts);
         }
 
-        if (!file_exists($opts['file']))
-        {
+        if (!file_exists($opts['file'])) {
             return array(
                 "success" => false,
-                "error" => "File `" . $opts['file'] . "` does not exist"
+                "error" => 'File `' . $opts['file'] . '` does not exist'
             );
         }
 
-        if( class_exists( 'CURLFile') ) {
-			$file = new CURLFile( $opts['file'] );
+        if (class_exists('CURLFile')) {
+			$file = new CURLFile($opts['file']);
 		} else {
 			$file = '@' . $opts['file'];
 		}
@@ -58,47 +51,44 @@ class Kraken
 
         $data = array_merge(array(
             "file" => $file,
-            "data" => json_encode(array_merge(
-                            $this->auth, $opts
-            ))
+            "data" => json_encode(array_merge($this->auth, $opts))
         ));
 
-        $response = self::request($data, "https://api.kraken.io/v1/upload");
+        $response = self::request($data, 'https://api.kraken.io/v1/upload', 'upload');
 
         return $response;
     }
 
-    public function status()
-    {
+    public function status() {
         $data = array('auth' => array(
             'api_key' => $this->auth['auth']['api_key'],
             'api_secret' => $this->auth['auth']['api_secret']
         ));
-        $response = self::request(json_encode($data), "https://api.kraken.io/user_status");
+
+        $response = self::request(json_encode($data), 'https://api.kraken.io/user_status', 'url');
 
         return $response;
     }
 
-    private function request($data, $url)
-    {
+    private function request($data, $url, $type) {
         $curl = curl_init();
+
+        if ($type === 'url') {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+            ));
+        }
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_FAILONERROR, 1);
+        curl_setopt($curl, CURLOPT_FAILONERROR, 0);
 
         $response = json_decode(curl_exec($curl), true);
-        $error = curl_errno($curl);
 
         curl_close($curl);
 
-        if ($error > 0) {
-            throw new RuntimeException(sprintf('cURL returned with the following error code: "%s"', $error));
-        }
-
         return $response;
     }
-
 }
